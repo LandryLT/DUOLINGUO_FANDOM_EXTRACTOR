8b3a588eeb446438e8bc7eb2bbf6723fdde0eb58
LandryLT <59315119+LandryLT@users.noreply.github.com>
2022-01-06 04:26:29 +0100

    fetch html on website

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+import urllib.request
+from os import cpu_count, listdir
+from os.path import isfile, join, abspath
 
+url = 'https://duonotes.fandom.com/wiki/Japanese'
+with urllib.request.urlopen(url) as response:
+    soup = BeautifulSoup(response.read(), 'html.parser')
+
+print(soup.prettify())
+
+# for div in soup.find_all('div'):
+#     if div['id'] == 'toc':
+#         print(div)
