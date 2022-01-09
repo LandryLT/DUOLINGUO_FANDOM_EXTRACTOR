df5137ba551e130181ba16d11cf941e890271855
LandryLT <59315119+LandryLT@users.noreply.github.com>
2022-01-10 00:35:29 +0100

    stlying modif

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ outputapkg = 'C:/Users/landr/Desktop/Tiiiiips.apkg'
 jap_fandom = FandomExtractWStyle(url)
 jap_duolingo_ankideck = GenkiDeck("Tiiiiips")
 
-jap_fandom.stylise("h2", 'color: red')
+jap_fandom.stylise("th", 'background-color: #ccc;text-align: left; border-bottom: 1px solid black')
+jap_fandom.stylise("tr", 'background-color: #eee;text-align: left; border-bottom: 1px solid black')
 extracted_fandom = FandomExtractWStyle.stringify_content(jap_fandom.content_dict)
 
 for chap in extracted_fandom:
